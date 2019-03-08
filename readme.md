14f3e23e2596c861830447db8e5d0583ba2e6600
Sascha Presnac <sascha.presnac@hausperfekt.de>
2019-03-08 09:53:57 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Laravel: Create a user from CLI with artisan
+
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+
+# Installation
+```
+composer require spresnac/laravel-create-user-cli
+```
---
+++ src/spresnac/createcliuser/CreateCliUserCommand.php
@@ -0,0 +1,65 @@
+<?php
+namespace spresnac\createcliuser;
+
+use \Illuminate\Console\Command;
+
+class CreateCliUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create 
+                                {name? : Inject the new users name} 
+                                {email? : Inject the new users email} 
+                                {password? : Inject the new users password} 
+                                {--force : Do not ask questions, just do it ;)}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a user from cli with artisan';
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $class = config(
+            'auth.providers.' . config(
+                'auth.guards.' . config(
+                    'auth.defaults.guard'
+                ) . '.provider'
+            ) . '.model'
+        );
+        $user = new $class;
+
+        $user->name = ($this->argument('name') === null)
+            ? $this->ask('User name: ')
+            : $this->argument('name');
+            
+        $user->email = ($this->argument('email') === null)
+            ? $this->ask('User email: ')
+            : $this->argument('email');
+
+        $user->email = ($this->argument('password') === null)
+            ? Hash::make($this->ask('User password: '))
+            : Hash::make($this->argument('password'));
+
+        if ($this->option('force') === true || $this->confirm('Save this user?', true)) {
+            $user->save();
+            $this->info('Created a user with id: ' . $user->id);
+        }
+        return true;
+    }
+}
