60c696d08d4299c6d27a2aa3a8dd47bba6016f2d
Thomas Spencer <tomspencer244@gmail.com>
2020-10-18 10:26:12 +0800

    Add tests
    Resolves spresnac/laravel-create-user-cli#5

---
+++ tests/spresnac/createcliuser/CreateCliUserCommandTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace spresnac\tests\createcliuser;
+
+use Orchestra\Testbench\TestCase;
+use spresnac\createcliuser\CreateCliUserCommandServiceProvider;
+
+/**
+ * Class CreateCliUserCommandTest
+ * @covers \spresnac\createcliuser\CreateCliUserCommand
+ * @covers \spresnac\createcliuser\CreateCliUserCommandServiceProvider
+ * @package spresnac\tests\createcliuser
+ */
+class CreateCliUserCommandTest extends TestCase
+{
+    public function testCanCreateUserWithNoArguments(): void
+    {
+        $expectedUserName = 'User Name';
+        $expectedUserEmail = 'user@email.com';
+        $expectedPassword = 'bad_password';
+
+        $this->artisan('user:create')
+            ->expectsQuestion('User name: ', $expectedUserName)
+            ->expectsQuestion('User email: ', $expectedUserEmail)
+            ->expectsQuestion('User password: ', $expectedPassword)
+            ->expectsQuestion('Save this user?', 'yes')
+            ->expectsOutput('Created a user with id: 1')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseHas('users', [
+            'email' => $expectedUserEmail,
+            'name' => 'User Name',
+        ]);
+    }
+
+    public function testCanCreateUserWithAllArguments(): void
+    {
+        $expectedUserName = 'User Name';
+        $expectedUserEmail = 'user@email.com';
+        $expectedPassword = 'bad_password';
+
+        $this->artisan(
+            sprintf(
+                'user:create --force "%s" %s %s',
+                $expectedUserName,
+                $expectedUserEmail,
+                $expectedPassword
+            )
+        )
+            ->expectsOutput('Created a user with id: 1')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseHas('users', [
+            'email' => $expectedUserEmail,
+            'name' => 'User Name',
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            CreateCliUserCommandServiceProvider::class,
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadLaravelMigrations();
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('auth.guards.web.provider', 'users');
+        $app['config']->set('auth.providers.users.model', User::class);
+    }
+}
